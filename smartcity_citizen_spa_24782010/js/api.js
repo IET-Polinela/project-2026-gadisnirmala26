2ded777c8ade9b44360d3afb388175182d10295e
gadisnirmala26 <nirmalagadis1@gmail.com>
2026-06-13 18:16:32 +0700

    Add GitHub Pages deployment

--- smartcity_citizen_spa_24782010/js/api.js
+++ smartcity_citizen_spa_24782010/js/api.js
@@ -3,6 +3,6 @@ async function requestAPI(endpoint, method = 'GET', bodyData = null) {
     const options = { method, headers: { 'Content-Type': 'application/json' } };
     if (token) options.headers['Authorization'] = `Bearer ${token}`;
     if (bodyData) options.body = JSON.stringify(bodyData);
-    const response = await fetch(`http://127.0.0.1:8000${endpoint}`, options);
+    const response = await fetch(`http://103.151.63.84:8002${endpoint}`, options);
     return response.json();
 }
